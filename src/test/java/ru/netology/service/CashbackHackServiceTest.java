18f67d405059f049cd1f86ac90c2df3b39fb977f
Ася Тамонова <asyatamoniva@192.168.1.161>
2022-09-04 18:04:09 +0300

    junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void ShouldReturnHundred(){
+        int expected = 100;
+        int actual = service.remain(900);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturnOne(){
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturnZeroThenZero(){
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturnZeroThenMore(){
+        int expected = 0;
+        int actual = service.remain(1001);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+}
